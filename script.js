6789fb5a4c0d56d78942c97ccf5c707379450968
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-09-09 15:43:49 -0500

    Part 2 commit

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ let iniPlant = 20;
 
 // Using this information, your objectives are to:
 // Predict the plant growth after a specific number of weeks.
-let n = 1; //n is number of weeks
+let n = 2; //n is number of weeks
 let growth = 20 * 2 ** (n - 1); // Plant growth after n-1 number of weeks because doubling starts from 2nd week
 let spaceTaken = growth * minSpace;
 
-console.log(`Radius: `, radius, `Area: `, area, `Number of weeks: `, n, `Number of plants: `,growth, `Total space taken: `, spaceTaken);
+console.log(`Radius: `, radius, `Area: `, area, `Number of weeks: `, n, `Number of plants: `, growth, `Total space taken: `, spaceTaken);
 
 // Implement control flow to make decisions on whether the plants should be:
 // Pruned, to stop them from exceeding the capacity of the garden.
@@ -49,3 +49,33 @@ else {
 
 // Within your submission, include the results for 1, 2, and 3 weeks of growth as inputs.
 //change the number of weeks accordingly at line 19 variable n.
+
+
+
+
+//PART 2
+
+// The amount of additional space that would be required if the scientists were to start with 100 plants, and did not prune them for 10 weeks.
+
+let newPlant = 100;
+let weekn = 10;
+let newGrowth = 100 * 2 ** (weekn - 1);
+
+let areaNeeded = 100 * minSpace * newGrowth;
+
+console.log(`Total area needed if scientists started with 100 plants with no pruning for 10 weeks:`, areaNeeded);
+
+let addAreaNeeded = areaNeeded - area;
+
+console.log(`Additional area needed:`, addAreaNeeded);
+
+// If the space remained circular, what would be the radius of this expanded garden?
+
+let newRadius = Math.sqrt(areaNeeded/PI);
+
+console.log(`Radius of expanded garden:`, newRadius);
+
+
+
+
+
